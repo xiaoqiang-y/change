e6fad1b4df0cad7a873269168791629a4fd821df
xiaoqiang-y <testsoftware@qq.com>
2020-02-01 16:27:27 +0800

    完成后台订单管理

--- WebApp/WebBackstage/Controllers/HomeController.cs
+++ WebApp/WebBackstage/Controllers/HomeController.cs
@@ -46,15 +46,84 @@ namespace WebBackstage.Controllers
             return View();
         }
 
-        public async Task<ActionResult> OrderManage()
+        public async Task<ActionResult> OrderManage(int States = -1)//查询订单
         {
             if (!ReLogin())
             {
                 return RedirectToAction("Index", "Login");
             }
-            var recvUser = await _context.Orders.ToListAsync();
+            ViewBag.States = States;
+            List<Orders> recvOrder = null;
+            if (States !=-1)
+            {
+                recvOrder = await _context.Orders.Where(p=>p.States == States).ToListAsync();
+            }
+            else
+            {
+                recvOrder = await _context.Orders.ToListAsync();
+            }
+            return View(recvOrder);
+        }
 
-            return View(recvUser);
+        public async Task<ActionResult> OrderDetail(string OrdersId)//订单详情
+        {
+            if (!ReLogin())
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            var recvOrder = await _context.Orders.Where(p=>p.OrdersId== OrdersId).ToListAsync();
+            var recvUser = await _context.Users.Where(p => p.UsersId == recvOrder[0].UsersId).ToListAsync();
+            var recvDelivery = await _context.Delivery.Where(p => p.DeliveryId == recvOrder[0].DeliveryId).ToListAsync();
+            var recvOrderD = await _context.OrdersDetail.Where(p => p.OrdersId == OrdersId).ToListAsync();
+            ViewData["OrdersId"] = recvOrder[0].OrdersId;
+            ViewData["UserName"] = recvUser[0].UserName;
+            ViewData["Orderdate"] = recvOrder[0].Orderdate;
+            ViewData["Total"] = recvOrder[0].Total;
+            ViewData["Consignee"] = recvDelivery[0].Consignee;
+            ViewData["Complete"] = recvDelivery[0].Complete;
+            ViewData["Phone"] = recvDelivery[0].Phone;
+            ViewData["States"] = recvOrder[0].States;
+            ViewData["Remark"] = recvOrder[0].Remark;
+
+            return View(recvOrderD);
+        }
+        public async Task<ActionResult> Shipments(string OrdersId)//订单发货
+        {
+            if (!ReLogin())
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            var recvOrder = await _context.Orders.FindAsync(OrdersId);
+            recvOrder.States = 2;//订单发货
+
+            _context.Update(recvOrder);
+            await _context.SaveChangesAsync();
+            
+            return RedirectToAction("OrderManage", "Home");
+
+            //return View(recvOrder);
+        }
+        public async Task<IActionResult> DeleteOrder(string OrdersId)//删除订单
+        {
+            if (OrdersId == null)
+            {
+                return NotFound();
+            }
+
+            var recvOrder = await _context.Orders.FindAsync(OrdersId);
+            double minutes = (DateTime.Now - recvOrder.Orderdate).TotalMinutes;
+            if (minutes > 60)//超过一个小时
+            {
+                var recvOrderD = await _context.OrdersDetail.Where(p => p.OrdersId == OrdersId).ToListAsync();
+                foreach (var item in recvOrderD)
+                {
+                    _context.OrdersDetail.Remove(item);
+                    await _context.SaveChangesAsync();
+                }
+                _context.Orders.Remove(recvOrder);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("OrderManage", "Home");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- WebApp/WebBackstage/Models/OrdersDetail.cs
+++ WebApp/WebBackstage/Models/OrdersDetail.cs
@@ -9,10 +9,10 @@ namespace WebBackstage.Models
         public string OrdersId { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
-        public int? States { get; set; }
+        public int States { get; set; }
         public string PhotoUrl { get; set; }
         public string Title { get; set; }
-        public decimal? Price { get; set; }
+        public decimal Price { get; set; }
 
         public virtual Orders Orders { get; set; }
         public virtual Product Product { get; set; }
